8d8469f1edb30549752e3d315b27be455b6b28f1
khushboo kumari <ksingh200601@gmail.com>
2025-03-15 23:40:37 +0530

    aimerz dsa solved

--- Day7-Problem3.js
+++ Day7-Problem3.js
@@ -10,3 +10,9 @@ let half=myPow(x,Math.floor(n/2))
 return n%2===0?half*half:x*half*half
 }
 console.log(myPow(2,5))
+
+// by manual method
+const poww =(a,b)=>{
+  return a**b
+}
+console.log(poww(2,5))
