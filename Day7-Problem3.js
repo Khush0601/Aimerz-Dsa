250158115e97904d263e0455bd9d61a28c84a64c
khushboo kumari <ksingh200601@gmail.com>
2025-03-15 23:37:28 +0530

    aimerz dsa solved

---
+++ Day7-Problem3.js
@@ -0,0 +1,12 @@
+const myPow=(x,n)=>{
+if(n===0){
+    return 1
+}
+if(n<0){
+    x=1/x;
+    n=-n
+}
+let half=myPow(x,Math.floor(n/2))
+return n%2===0?half*half:x*half*half
+}
+console.log(myPow(2,5))
