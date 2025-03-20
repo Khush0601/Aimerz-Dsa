ea95e07cd6c6b5eafbb05314dc24feb59ac80244
khushboo kumari <ksingh200601@gmail.com>
2025-03-20 12:28:50 +0530

    alternate Sort

---
+++ Day11-Problem1.js
@@ -0,0 +1,19 @@
+function alternativeSort(arr){
+    arr.sort((a,b)=>a-b)
+    let i=0;j=arr.length-1;
+    let result=[]
+    while(i<=j){
+        if(j>=1){
+            result.push(arr[j])
+            j--
+        }
+        if(i<=j){
+            result.push(arr[i])
+            i++
+        }
+    }
+    console.log(result.join(" "))
+
+}
+let arr1=[2,34,5,44,67,4,56]
+alternativeSort(arr1)
