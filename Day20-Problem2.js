7288ac47e2f78a5f0ed0ec81f62211f5c5f00c3d
khushboo kumari <ksingh200601@gmail.com>
2025-03-28 20:25:52 +0530

    print frequencies without extra space

---
+++ Day20-Problem2.js
@@ -0,0 +1,17 @@
+// given array ,print frequencies without extra space
+
+function printFrequencies(arr){
+  arr.sort((a,b)=>a-b);
+
+  let count=1;
+  for(let i=1;i<=arr.length;i++){
+    if(i<arr.length && arr[i]===arr[i-1]){
+        count++;
+    }
+    else{
+        console.log(arr[i-1]+" occured "+count);
+        count=1;
+    }
+  }
+}
+console.log(printFrequencies([4,2,2,8,4,4,8,8]))
