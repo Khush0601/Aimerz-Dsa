9d0a0ce460dbb554de1b3ba56dd31b6709a550d9
khushboo kumari <ksingh200601@gmail.com>
2025-04-01 19:30:49 +0530

    1st and last occurence of an element in a sorted array

--- Day23-Problem4.js
+++ Day23-Problem4.js
@@ -1,12 +1,25 @@
 //find the 1st and last occurence of an element in a sorted array:
 
 function findFirstAndLast(arr, target) {
-    let firstIndex=-1
-  for(let i=0;i<arr.length;i++){
-   if(firstIndex===-1){
-     firstIndex=i
-   }
-  }
-  return firstIndex
+    let first = -1, last = -1;
+ for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            if (first === -1){
+                first = i; 
+            }
+            last = i; 
+        }
+    }
+
+    return [first,last];
+}
+console.log(findFirstAndLast([2, 4, 4, 4, 6, 7, 8], 4))
+
+
+// alternate method:
+function findFirstAndLast(arr, target) {
+   let first=arr.indexOf(target)
+   let last=arr.lastIndexOf(target)
+   return [first,last]
 }
 console.log(findFirstAndLast([2, 4, 4, 4, 6, 7, 8], 4))
