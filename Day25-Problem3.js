64a8295a87501cf9bea350fd39a1f6727292cfba
khushboo kumari <ksingh200601@gmail.com>
2025-04-02 09:35:56 +0530

    majority el in array

---
+++ Day25-Problem3.js
@@ -0,0 +1,20 @@
+// two non-repeating el 
+function majority(arr){
+    let freq={}
+    let majorityCount=Math.ceil(arr.length/2);
+    for(let i=0;i<arr.length;i++){
+       if(freq[arr[i]]){
+           freq[arr[i]]++
+       }
+       else{
+           freq[arr[i]]=1
+       }
+    }
+    for(let key in freq){
+     if(freq[key]>=majorityCount){
+        return Number(key)
+     }
+    }
+    return -1
+   }
+   console.log(majority([3,3,4,2,4,4,2,4,4]))
