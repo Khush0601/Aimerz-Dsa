dfff396d4f67348d76159685de0cfcafd9993a75
khushboo kumari <ksingh200601@gmail.com>
2025-04-19 23:00:34 +0530

    move last el to front

--- Day30-Problem1.js
+++ Day30-Problem1.js
@@ -1,8 +1,6 @@
-// linkedList
-
-//Place Even Nodes Before Odd Nodes
-//Rearrange a Linked List so that All Even Numbers Appear Before All Odd Numbers (Maintain Original Order)
-
+// linked list questions
+// write a fun. that moves the last el. to the front in a given liknked list:
+//step1:node class:
 class Node {
     constructor(data) {
       this.data = data;
@@ -10,79 +8,45 @@ class Node {
     }
   }
   
+//steo2:fun. to print linked list:
 
-function rearrangeEvenOdd(head) {
-    if (!head) {
-        return null;
-    }
-  
-    let evenHead = null, evenTail = null;
-    let oddHead = null, oddTail = null;
-  
-    let current = head;
-  
-    while (current !== null) {
-      if (current.data % 2 === 0) {
-        // Even number
-        if (evenHead === null) {
-          evenHead = current;
-          evenTail = current;
-        } 
-        else {
-          evenTail.next = current;
-          evenTail = evenTail.next;
-        }
-      } 
-      else {
-        // Odd number
-        if (oddHead === null) {
-          oddHead = current;
-          oddTail = current;
-        } else {
-          oddTail.next = current;
-          oddTail = oddTail.next;
-        }
-      }
-      current = current.next;
-    }
-  
-    // Join even list to odd list
-    if (evenTail !== null) {
-      evenTail.next = oddHead;
-    }
-  
-    // End the final list properly
-    if (oddTail !== null) {
-      oddTail.next = null;
-    }
-  
-    return evenHead !== null ? evenHead : oddHead;
-  }
-  
-  
-// Helper function to print list
 function printList(head) {
     let current = head;
-    let output = '';
-    while (current) {
-      output += current.data + ' → ';
+    let result = '';
+    while (current!==null){
+     result+= current.data + ' → ';
       current = current.next;
     }
-    console.log(output + 'null');
+    console.log(result + 'null');
   }
-  
-  let head = new Node(1);
-  head.next = new Node(2);
-  head.next.next = new Node(3);
-  head.next.next.next = new Node(4);
-  head.next.next.next.next = new Node(5);
-  head.next.next.next.next.next = new Node(6);
-  
-  let newHead = rearrangeEvenOdd(head);
-  printList(newHead);
 
+//step3:fun. that moves last el to front:
+function moveLastToFront(head){
+    if(head===null || head.next===null){
+        return head;
+    }
+    let secondLast=null;
+    let last=head;
 
+    while(last.next!==null){
+        secondLast=last;
+        last=last.next;
+    }
+    secondLast.next=null;
+    last.next=head;
+    head=last;
+    return head;
+}
 
+//step4:create linked list:
+  let head = new Node(10);
+  head.next = new Node(20);
+  head.next.next = new Node(30);
+  head.next.next.next = new Node(40);
   
-  
-  
+  console.log('original linked list')
+  printList(head);
+
+  console.log('after moving last to front ')
+  printList(head);
+
