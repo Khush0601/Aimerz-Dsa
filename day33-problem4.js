b84bd0da7aa41a848035c6dc50b94a2912a9c06e
khushboo kumari <ksingh200601@gmail.com>
2025-04-21 17:18:15 +0530

    check the linked list of strings from a palindrome

---
+++ day33-problem4.js
@@ -0,0 +1,50 @@
+// check the linked list of strings from a palindrome:
+
+//step1:class node
+class Node {
+    constructor(data) {
+      this.data = data;
+      this.next = null;
+    }
+  }
+
+//step2:function of addone:
+function isPalindromeList(head){
+    let current = head;
+  let result = "";
+
+  while (current) {
+    result += current.data;
+    current = current.next;
+  }
+
+  let reversed = result.split('').reverse().join('');
+  return reversed === result;
+}
+
+
+//step4:function to print:
+
+function printList(head) {
+    let current = head;
+    let result = '';
+    while (current){
+     result+= current.data + ' → ';
+      current = current.next;
+    }
+    console.log(result + 'null');
+ }
+
+ //step5:linked list
+let head = new Node("abc");
+head.next = new Node("abc");
+head.next.next = new Node("cba");
+head.next.next.next = new Node("cba");
+
+
+
+console.log("original list");
+printList(head);
+
+console.log(isPalindromeList(head))
+
